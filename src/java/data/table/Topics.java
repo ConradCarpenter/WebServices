1b80d8b808dc6ecaa2d6d37424ab1fd6108cc5c5
Zachary Jones <zjones@hartford.edu>
2015-12-14 12:53:50 -0500

    add topics link arrays

---
+++ src/java/data/table/Topics.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Jones.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package data.table;
+
+/**
+ *
+ * @author Jones
+ */
+public class Topics {
+    String[] sports = {"http://espn.go.com/nfl/insider/story/_/id/14305414/how-running-back-woes-hampered-dallas-cowboys-nfl",
+    "http://espn.go.com/college-football/story/_/id/14315752/derrick-henry-christian-mccaffrey-deshaun-watson-named-heisman-trophy-finalists",
+    "http://espn.go.com/nba/story/_/id/14314807/lebron-james-signs-life-deal-nike",
+    "http://espn.go.com/nba/story/_/id/14311036/kristaps-porzingis-new-york-knicks-looks-forward-matchup-dirk-nowitzki-dallas-mavericks",
+    "http://espn.go.com/nba/powerrankings/_/week/6"};
+    
+    String[] gadgets = {"http://www.techtimes.com/articles/113457/20151205/xiaomi-launches-its-first-product-in-the-us-sub-100-yi-action-camera-available-now-on-amazon.htm",
+    "http://mashable.com/2015/12/05/giant-drone-transports-humans/?utm_cid=mash-com-fb-tech-link#0A93wKkVU5qs",
+    "http://techcrunch.com/2015/12/04/gopro-further-muddles-its-product-line-with-hero4-session-price-drop/",
+    "http://www.technologyreview.com/news/544166/smart-bandage-signals-infection-by-turning-fluorescent/",
+    "http://gadgetshow.channel5.com/news/clarkson-explains-how-amazon-drone-delivery-works-new-ad"};
+    
+    String[] gaming = {"http://www.ign.com/articles/2015/12/07/daily-deals-star-wars-battlefront-ps4-bundle-anime-sale-rise-of-the-tomb-raider?utm_source=IGN%20hub%20page&utm_medium=IGN%20(front%20page)&utm_content=2&utm_campaign=Coverstory",
+    "http://www.ign.com/articles/2015/12/07/fallout-4-should-be-the-last-fallout?utm_source=IGN%20hub%20page&utm_medium=IGN%20(front%20page)&utm_content=4&utm_campaign=Coverstory",
+    "http://www.ign.com/articles/2015/12/04/new-content-revealed-for-halo-5?utm_source=IGN%20hub%20page&utm_medium=Xbox%20One&utm_content=2&utm_campaign=Spotlight",
+    "http://www.ign.com/articles/2015/11/30/rainbow-six-siege-review?utm_source=IGN%20hub%20page&utm_medium=PC&utm_content=2&utm_campaign=Coverstory",
+    "http://www.ign.com/articles/2015/12/04/telltale-reveals-batman-game?utm_source=IGN%20hub%20page&utm_medium=Wii%20U&utm_content=14&utm_campaign=Blogroll"};
+    
+    String[] music = {"http://www.usatoday.com/story/life/music/2015/12/07/snubs-surprises-grammy-nominations-2016/76908840/",
+    "http://www.theverge.com/2015/12/5/9851482/apple-music-streaming-wars-spotify-youtube",
+    "http://www.mtv.com/news/2682345/drake-back-to-back-diss-grammy-nomination/",
+    "http://www.ibtimes.co.uk/adele-accused-stealing-song-popular-kurdish-singer-ahmet-kaya-1532177",
+    "http://www.avclub.com/article/why-mariah-careys-christmas-hit-will-be-around-for-229110"};
+    
+    String[] food = {"http://www.cookingchanneltv.com/recipes/holiday-christmas-appetizers.html",
+    "http://www.cookingchanneltv.com/recipes/corys-famous-mac-and-cheese.html",
+    "http://www.cookingchanneltv.com/recipes/g-garvin/white-chocolate-peppermint-cookies.html",
+    "http://blog.cookingchanneltv.com/2015/12/07/best-christmas-tv-episodes/",
+    "http://www.cookingchanneltv.com/recipes/best-holiday-cookie-recipes.html"};
+}
+
